2238afd72b008965f845e27b228611f4820dba66
RR <korvy85@gmail.com>
2024-03-05 22:31:48 +0200

    fix err task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,22 +1,9 @@
-// const itemCategories = document.querySelectorAll('li.item');
-// console.log(`Number of categories: ${itemCategories.length}`);
-
-
-
-
-// itemCategories.forEach((el) => {
-//     console.log(
-//         `Category: ${el.firstElementChild.textContent}\n Elements: ${el.lastElemetChild.children.length}`
-//     );
-// });
-
-
 const categoryItemsEl = document.querySelectorAll('li.item');
 
 console.log(`Number of categories: ${categoryItemsEl.length}`);
 
 categoryItemsEl.forEach((el) => {
     console.log(
-        `Category: ${el.firstElementChild.textContent}\n Elements: ${el.lastElemetChild.children.length}`
+        `Category: ${el.firstElementChild.textContent}\nElements: ${el.lastElementChild.children.length}`
     );
 });
