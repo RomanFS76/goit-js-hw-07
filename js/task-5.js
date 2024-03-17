cace0cc6db7f82a086f3cb07f75fb9c494cfcf14
RR <korvy85@gmail.com>
2024-03-17 15:31:45 +0200

    fix err tsk5

--- js/task-5.js
+++ js/task-5.js
@@ -11,7 +11,9 @@ const spanColorEl = document.querySelector('span.color');
 
 btnChangeColor.addEventListener('click', changeColor);
 
+
 function changeColor() { 
-  spanColorEl.textContent = getRandomHexColor();
-  document.body.style.backgroundColor = getRandomHexColor();
+  const getColor = getRandomHexColor();
+  spanColorEl.textContent = getColor;
+  document.body.style.backgroundColor = getColor;
 };
